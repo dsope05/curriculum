df715a8a006b538f97b152d6d81220bcf9aea1b9
dsope05 <dsoper@llip.io>
2018-06-28 14:46:25 -0700

    end to end chat room prob 6

--- curriculum/end2end/chat.js
+++ curriculum/end2end/chat.js
@@ -6,6 +6,10 @@ app.listen(6875, () => console.log('listening on 6875'));
 
 const messages = [];
 
+app.get('/chat', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
 app.get('/newMessage', (req, res) => {
   console.log('reqqyery', req.query);
   const { name, message } = req.query;
@@ -13,7 +17,7 @@ app.get('/newMessage', (req, res) => {
   res.json(messages);
 });
 
-app.get('/messages', (req, res) => {
+app.get('/getMessages', (req, res) => {
   res.json(messages);
-})
+});
 
