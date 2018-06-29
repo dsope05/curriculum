b8bdc047375bff4b8125733977080c447e5dece8
dsope05 <dsoper@llip.io>
2018-06-28 19:49:36 -0700

    end to end prob 7 chat

--- curriculum/end2end/chat.js
+++ curriculum/end2end/chat.js
@@ -1,23 +1,47 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
 // All your code should go here
 
+app.use(cookieParser());
 app.listen(6875, () => console.log('listening on 6875'));
 
 const messages = [];
 
 app.get('/chat', (req, res) => {
+  if (!req.cookies.name) {
+    return res.redirect('/login');
+  }
   res.sendFile(__dirname + '/public/index.html');
 });
 
+app.get('/login', (req, res) => {
+  if (req.query.name) {
+    res.cookie('name', req.query.name);
+  }
+  res.sendFile(__dirname + '/public/login.html');
+});
+
+
 app.get('/newMessage', (req, res) => {
-  console.log('reqqyery', req.query);
   const { name, message } = req.query;
   messages.push({ name, message });
   res.json(messages);
 });
 
 app.get('/getMessages', (req, res) => {
-  res.json(messages);
+  if (req.cookies.name) {
+    return res.json(messages);
+  }
+  return res.status(400);
 });
 
+app.get('/getUser', (req, res) => {
+   return res.json(req.cookies.name);
+});
+
+
+app.get('/logout', (req, res) => {
+   res.cookie('name', '');
+  return res.send('ok');
+});
